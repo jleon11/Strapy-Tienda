35ec72d9c9462418b03ab2a1f2d8e7778be54055
Justin Leon Perez <justinleon1111@gmail.com>
2026-05-11 10:38:13 -0600

    ajustes

---
+++ src/api/subcriptor/controllers/subcriptor.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * subcriptor controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::subcriptor.subcriptor');
---
+++ src/api/subcriptor/services/subcriptor.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * subcriptor service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::subcriptor.subcriptor');
